16b746482784669fa867ac8cb7b1a609902c5443
Karol <grydzkarol@gmail.com>
2019-11-27 14:53:53 +0100

    CREATE LOGIN

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, FormControl, InputLabel, Input } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <FormControl>
+        <InputLabel htmlFor="username">username</InputLabel>
+        <Input id="username" />
+
+        <InputLabel htmlFor="password">password</InputLabel>
+        <Input id="password" />
+      </FormControl>
+    </Container>
   );
 }
 
