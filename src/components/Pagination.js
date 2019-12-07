b92045834662ad6adac311d80d7f7af846325920
Karol Grydz <grydzkarol@gmail.com>
2019-12-07 13:35:34 +0100

    PAGINATION

---
+++ src/components/Pagination.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Pagination = () => {
+  return (
+    <nav>
+      <ul className="pagination">
+        {pageNumbers.map(number => (
+          <li key={number} className="page-item">
+            <a onClick={() => paginate(number)} href="!#" className="page-link">
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
